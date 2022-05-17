5888b32de019051af8a5c3ecdb8521081af43f96
SuneelM <python.linux789@gmail.com>
2022-05-17 00:02:47 -0400

    First commit

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AndroidRuntimeException
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
